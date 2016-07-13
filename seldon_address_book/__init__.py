312930300f23e85d95ec21c1dd37557d1502084f
Justin Wilson <justinleewilson@gmail.com>
2016-07-13 20:53:13 +0100

    enable CORS

--- seldon_address_book/__init__.py
+++ seldon_address_book/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.secret_key = 'seldonplan'
+
+CORS(app)
 
 db = SQLAlchemy(app)
 
