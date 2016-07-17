779ade77dd3409f55a7125c3d7040683fadb31a0
Justin Wilson <justinleewilson@gmail.com>
2016-07-17 19:53:40 +0100

    sorted api to return correct links for ids

--- seldon_address_book/api.py
+++ seldon_address_book/api.py
@@ -2,13 +2,20 @@ from flask import request, jsonify
 from seldon_address_book import app, db
 from seldon_address_book.models import Organisation, Person
 
+# TODO : move into config
+HOST = 'http://localhost:8080'
+
 @app.route('/', methods=['GET'])
 def discover():
     response = jsonify({
-        "links": [
+        'links': [
             {
-                "rel": "organisations",
-                "href": "http://localhost:8080/organisations"
+                'rel': 'self',
+                'href': HOST
+            },
+            {
+                'rel': 'organisations',
+                'href': HOST + '/organisations'
             }
         ]
     })
@@ -18,7 +25,17 @@ def discover():
 @app.route('/organisations', methods=['GET'])
 def retrieve_organisations():
     organisations = Organisation.query.all()
-    response = jsonify({"organisations" : [o.serialise() for o in organisations]})
+    organisations_array = [o.serialise() for o in organisations];
+    organisations_with_links = _organisations_with_links(organisations_array)
+    response = jsonify({
+        'organisations' : organisations_with_links,
+        'links': [
+            {
+                'rel': 'self',
+                'href': HOST + '/organisations'
+            }
+        ]
+    })
     response.status_code = 200
     return response
 
@@ -28,41 +45,48 @@ def create_organisation():
     organisation = _create_organisation(payload)
     db.session.add(organisation)
     db.session.commit()
-    response = jsonify({
-        "links": [
-            {
-                "rel": str(organisation.id),
-                "href": "/organisations/" + str(organisation.id)
-            },
-            {
-                "rel": "add-person",
-                "href": "/organisations/" + str(organisation.id) + "/persons"
-            }
-        ]
-    })
+    response = jsonify({})
     response.status_code = 201
-    response.headers['location'] = '/organisations/' + str(organisation.id)
+    response.headers['location'] = HOST + '/organisations/' + str(organisation.id)
     return response
 
 @app.route('/organisations/<int:organisation_id>', methods=['GET'])
 def retrieve_organisation(organisation_id):
     organisation = Organisation.query.get(organisation_id)
-    response = jsonify({"organisation": organisation.serialise()})
+    response = jsonify({
+        'organisation': organisation.serialise(),
+        'links': [
+            {
+                'rel': 'self',
+                'href': HOST + '/organisations/' + str(organisation_id)
+            }
+        ]
+    })
     response.status_code = 200
+    return response
+
+@app.route('/organisations/<int:organisation_id>', methods=['DELETE'])
+def delete_organisation(organisation_id):
+    organisation = Organisation.query.get(organisation_id)
+    db.session.delete(organisation)
+    db.session.commit()
+    response = jsonify({})
+    response.status_code = 204
     return response
 
 @app.route('/organisations/<int:organisation_id>', methods=['PUT'])
 def update_organisation(organisation_id):
     organisation = Organisation.query.get(organisation_id)
     payload = request.get_json();
+    print(payload);
     organisation.name = payload['name']
     organisation.address = payload['address']
     organisation.phone = payload['phone']
+    organisation.persons = _persons_from(payload['persons'])
     db.session.commit()
-    response = jsonify({"organisation": organisation.serialise()})
+    response = jsonify({'organisation': organisation.serialise()})
     response.status_code = 200
     return response
-
 
 @app.route('/organisations/<int:organisation_id>/persons', methods=['POST'])
 def create_person(organisation_id):
@@ -74,17 +98,30 @@ def create_person(organisation_id):
     db.session.add(organisation)
     db.session.commit()
     response = jsonify({
-        "links": [
+        'links': [
             {
-                "rel": str(person.id),
-                "href": "/organisations/" + str(organisation_id) + "/persons/" + str(person.id)
+                'rel': str(person.id),
+                'href': '/organisations/' + str(organisation_id) + '/persons/' + str(person.id)
             }
         ]
     })
     response.status_code = 201
-    response.headers['location'] = "/organisations/" + str(organisation_id) + "/persons/" + str(person.id)
+    response.headers['location'] = '/organisations/' + str(organisation_id) + '/persons/' + str(person.id)
     return response    
 
+
+def _organisations_with_links(organisations):
+    return [_add_link(o) for o in organisations]
+
+def _add_link(organisation):
+    organisation['id'] = {
+        'rel': 'self',
+        'href': HOST + '/organisations/' + str(organisation['id'])
+    }
+    return organisation
+    
+def _persons_from(persons):
+    return [Person(p['forename'], p['surname']) for p in persons]
 
 def _create_person(payload):
     return Person(payload['forename'], payload['surname'])
